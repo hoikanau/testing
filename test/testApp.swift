327adaa9d3a90a81c6125fdc0f09bc11f56788e5
hoikanau <hoikanau123@gmail.com>
2025-12-02 22:22:27 -0800

    Initial commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Jerry Au on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
